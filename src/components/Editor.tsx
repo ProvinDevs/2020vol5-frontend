acbf2a742b51b6951d5c7caa2841b1141c29dd69
Shuntaro Nishizawa <me@shun.technology>
2021-03-18 01:38:27 +0900

    feat: add editor animation loop

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,19 +1,33 @@
 import type { FC } from "react";
 import { useRef, useEffect } from "react";
 
-import { assertNonNull } from "../utils/assert";
+import { Scene } from "../lib/editor/Scene";
+import { Background } from "../lib/editor/objects";
 
-const Editor: FC = () => {
+type Props = {
+  backgroundImagePath: string;
+};
+const Editor: FC<Props> = ({ backgroundImagePath }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas === null) return;
-    const ctx = canvas.getContext("2d");
-    assertNonNull(ctx, "ctx");
+
+    const scene = new Scene(canvas);
+    const background = new Background(canvas, backgroundImagePath);
+    scene.add(background);
 
     // TODO: 追加実装
-  }, [canvasRef]);
+
+    let requestId: number;
+    const animate = () => {
+      scene.draw();
+      requestId = requestAnimationFrame(animate);
+    };
+    animate();
+    return () => cancelAnimationFrame(requestId);
+  }, [canvasRef, backgroundImagePath]);
 
   return <canvas ref={canvasRef} />;
 };
