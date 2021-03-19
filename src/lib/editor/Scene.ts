f3ce0ac0b45e18b00a13cbec0fe8b054794f0cc5
Shuntaro Nishizawa <me@shun.technology>
2021-03-19 18:27:29 +0900

    feat: add super class to Scene

--- src/lib/editor/Scene.ts
+++ src/lib/editor/Scene.ts
@@ -1,30 +1,18 @@
-import { Drawable } from "./objects";
+import { Group } from "./objects";
 import { assertNonNull } from "../../utils/assert";
 
-export class Scene {
+export class Scene extends Group {
   private readonly _ctx: CanvasRenderingContext2D;
-  private _children: Array<Drawable> = [];
 
   constructor(public readonly canvas: HTMLCanvasElement) {
+    super();
     const ctx = canvas.getContext("2d");
     assertNonNull(ctx, "ctx");
     this._ctx = ctx;
   }
 
-  get children(): ReadonlyArray<Drawable> {
-    return this._children;
-  }
-  add(obj: Drawable): this {
-    this._children.push(obj);
-    return this;
-  }
-  remove(obj: Drawable): this {
-    this._children = this._children.filter((object) => Object.is(object, obj));
-    return this;
-  }
-
   draw(): void {
     this._ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this._children.forEach((obj) => obj.draw(this._ctx));
+    super.draw(this._ctx);
   }
 }
