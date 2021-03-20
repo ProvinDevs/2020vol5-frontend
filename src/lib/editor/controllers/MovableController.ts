c6b896cb98930cdc2c931e9dd3ab457d585e937a
Shuntaro Nishizawa <me@shun.technology>
2021-03-21 00:24:28 +0900

    feat: add object moving

--- src/lib/editor/controllers/MovableController.ts
+++ src/lib/editor/controllers/MovableController.ts
@@ -1,18 +1,19 @@
 import { Group, Movable } from "../objects";
 import { Vector2 } from "../math";
 
-type MovingObjectInfo = {
-  object: Movable;
-  offset: Vector2;
-};
-
 export class MovableController {
-  movingObjectInfo?: MovingObjectInfo;
+  selectedObject?: Movable;
+  clickOffset?: Vector2;
+  isClick = false;
 
   constructor(
     private canvas: HTMLCanvasElement,
     private movables: Group<Movable>,
   ) {
+    this.handleMousedown = this.handleMousedown.bind(this);
+    this.handleMousemove = this.handleMousemove.bind(this);
+    this.handleMouseup = this.handleMouseup.bind(this);
+
     canvas.addEventListener("mousedown", this.handleMousedown, false);
     canvas.addEventListener("mousemove", this.handleMousemove, false);
     canvas.addEventListener("mouseup", this.handleMouseup, false);
@@ -37,14 +38,47 @@ export class MovableController {
   }
 
   private handleMousedown(event: MouseEvent): void {
-    // TODO: 実装
+    this.isClick = true;
+    const clickedPos = this.getClickPoint(event);
+
+    if (this.selectedObject !== undefined) {
+      this.clickOffset = this.selectedObject.position.clone().sub(clickedPos);
+      return;
+    }
+
+    const topClickedObj = this.getClickedObjects(clickedPos).slice(-1)[0];
+    if (topClickedObj === undefined) return;
+
+    this.movables.remove(topClickedObj);
+    this.movables.add(topClickedObj);
+    this.selectedObject = topClickedObj;
+    this.clickOffset = topClickedObj.position.clone().sub(clickedPos);
   }
 
   private handleMousemove(event: MouseEvent): void {
-    // TODO: 実装
+    this.isClick = false;
+    if (this.selectedObject === undefined || this.clickOffset === undefined) {
+      return;
+    }
+
+    const clickedPos = this.getClickPoint(event);
+    this.selectedObject.position = clickedPos.clone().add(this.clickOffset);
   }
 
   private handleMouseup(event: MouseEvent): void {
-    // TODO: 実装
+    this.clickOffset = undefined;
+    if (!this.isClick) return;
+
+    const clickedPos = this.getClickPoint(event);
+    const topClickedObjWithoutSelected = this.getClickedObjects(clickedPos)
+      .filter((obj) => !Object.is(obj, this.selectedObject))
+      .slice(-1)[0];
+    if (topClickedObjWithoutSelected === undefined) {
+      this.selectedObject = undefined;
+      return;
+    }
+    this.movables.remove(topClickedObjWithoutSelected);
+    this.movables.add(topClickedObjWithoutSelected);
+    this.selectedObject = topClickedObjWithoutSelected;
   }
 }
