93bd8694a763cfaaea588dfa8256488da4d81a18
Shuntaro Nishizawa <me@shun.technology>
2021-03-22 15:30:37 +0900

    feat: add draw rotate handle

--- src/lib/editor/objects/SelectBox.ts
+++ src/lib/editor/objects/SelectBox.ts
@@ -1,5 +1,6 @@
 import { Drawable } from "./Drawable";
 import { Movable } from "./Movable";
+import { Vector2 } from "../math";
 
 export class SelectBox implements Drawable {
   constructor(public selectedObject?: Movable) {}
@@ -8,12 +9,32 @@ export class SelectBox implements Drawable {
     if (this.selectedObject === undefined) return;
     ctx.translate(...this.selectedObject.position.toArray());
     ctx.rotate(this.selectedObject.angle);
-    const offset = this.selectedObject.size.clone().div(2).negate().toArray();
+    const [x, y] = this.selectedObject.size.clone().div(2).toArray();
+    const vertex: [Vector2, Vector2, Vector2, Vector2] = [
+      new Vector2(-x, -y),
+      new Vector2(x, -y),
+      new Vector2(x, y),
+      new Vector2(-x, y),
+    ];
+
+    // 枠の描画
     ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.strokeStyle = "black";
-    ctx.rect(...offset, ...this.selectedObject.size.toArray());
+    ctx.rect(...vertex[0].toArray(), ...this.selectedObject.size.toArray());
     ctx.stroke();
+
+    // 各頂点のリサイズハンドルの描画
+    vertex.forEach((point) => {
+      ctx.beginPath();
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "black";
+      ctx.arc(...point.toArray(), 20, 0, 2 * Math.PI);
+      ctx.fillStyle = "white";
+      ctx.fill();
+      ctx.stroke();
+    });
+
     ctx.rotate(-this.selectedObject.angle);
     ctx.translate(...this.selectedObject.position.clone().negate().toArray());
   }
