2c4e8e98bcf79f4d4f1a403cd864485a49a47163
Shuntaro Nishizawa <me@shun.technology>
2021-03-24 22:39:39 +0900

    feat: add router (#8)

---
+++ src/pages/Take.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import type { BrowserRouterProps } from "react-router-dom";
+
+const Take: FC<BrowserRouterProps> = () => {
+  return (
+    <div>
+      <h1>This is Take page.</h1>
+    </div>
+  );
+};
+
+export default Take;
