2cacfd01b66d50bc6e6347f70beb491fb411659b
Shuntaro Nishizawa <me@shun.technology>
2021-03-25 04:34:29 +0900

    feat: create new room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,18 +13,8 @@ const Home: FC<BrowserRouterProps> = () => {
 
   const { setStore } = useStore();
 
-  const handleClick = async () => {
-    const apiUrl = process.env["REACT_APP_API_URL"];
-    assertNonNull(apiUrl);
-    const client = new GrpcApiClient(apiUrl);
-    const roomId = Number.parseInt(strRoomId, 10);
-    if (isNaN(roomId)) {
-      throw new Error("roomId must be non NaN");
-    }
-    const signallingStream = await client.joinRoom(
-      Number.parseInt(strRoomId, 10),
-    );
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
+  const getMediaStream = (): Promise<MediaStream> => {
+    return navigator.mediaDevices.getUserMedia({
       video: {
         width: { max: 1920 },
         height: { max: 1080 },
@@ -32,19 +22,51 @@ const Home: FC<BrowserRouterProps> = () => {
         facingMode: "user",
       },
     });
+  };
+
+  const handleCreateClick = async () => {
+    const apiUrl = process.env["REACT_APP_API_URL"];
+    assertNonNull(apiUrl);
+    const client = new GrpcApiClient(apiUrl);
+
+    const { roomId } = await client.createNewRoom();
+    const signallingStream = await client.joinRoom(roomId);
+
+    const mediaStream = await getMediaStream();
     setStore({ mediaStream, signallingStream });
+
+    history.push("/edit");
+  };
+
+  const handleJoinClick = async () => {
+    const apiUrl = process.env["REACT_APP_API_URL"];
+    assertNonNull(apiUrl);
+    const client = new GrpcApiClient(apiUrl);
+
+    const roomId = Number.parseInt(strRoomId, 10);
+    if (isNaN(roomId)) {
+      throw new Error("roomId must be non NaN");
+    }
+    const signallingStream = await client.joinRoom(roomId);
+
+    const mediaStream = await getMediaStream();
+    setStore({ mediaStream, signallingStream });
+
     history.push("/edit");
   };
 
   return (
     <div>
       <div>
+        <button onClick={handleCreateClick}>新規ルーム作成</button>
+      </div>
+      <div>
         <span>ルームID</span>
         <input
           value={strRoomId}
           onChange={({ target }) => setStrRoomId(target.value)}
         />
-        <button onChange={handleClick}>参加</button>
+        <button onClick={handleJoinClick}>参加</button>
       </div>
     </div>
   );
