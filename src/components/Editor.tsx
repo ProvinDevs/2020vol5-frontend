7b742a03fa3d0181a05024a82fb27ac2e01f2a2d
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-03-26 00:45:10 +0900

    feat: home button

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,19 +1,22 @@
 import type { FC } from "react";
 import { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import { Scene } from "../lib/editor/Scene";
 import { Background } from "../lib/editor/objects";
-import { MovableController } from "../lib/editor/controllers/MovableController";
 import { StampFactory } from "../lib/editor/factory/StampFactory";
+import { MovableController } from "../lib/editor/controllers/MovableController";
+import { HomeButtonController } from "../lib/editor/controllers/HomeButtonController";
+import { EmojiPaletteController } from "../lib/editor/controllers/EmojiPaletteController";
 
 import styles from "./Editor.module.scss";
-import { EmojiPaletteController } from "../lib/editor/controllers/EmojiPaletteController";
 
 type Props = {
   backgroundImagePath: string;
 };
 const Editor: FC<Props> = ({ backgroundImagePath }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const history = useHistory();
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -26,14 +29,14 @@ const Editor: FC<Props> = ({ backgroundImagePath }) => {
     const stampController = new MovableController(canvas);
     scene.add(stampController.movables);
     scene.add(stampController.selectBox);
-
     const emojiPalette = new EmojiPaletteController(
       canvas,
       stampFactory,
       stampController,
     );
-
     scene.add(emojiPalette.objects);
+    const homeButton = new HomeButtonController(canvas, history);
+    scene.add(homeButton.objects);
 
     stampController.on("add", console.log);
     stampController.on("change", console.log);
