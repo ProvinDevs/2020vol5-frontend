696f709a9c7604007cf14cc94cab4d6e95ee9549
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-03-25 09:49:36 +0900

    remove: test stamps

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,7 +2,6 @@ import type { FC } from "react";
 import { useRef, useEffect } from "react";
 
 import { Scene } from "../lib/editor/Scene";
-import { Vector2 } from "../lib/editor/math";
 import { Background } from "../lib/editor/objects";
 import { MovableController } from "../lib/editor/controllers/MovableController";
 import { StampFactory } from "../lib/editor/factory/StampFactory";
@@ -39,19 +38,6 @@ const Editor: FC<Props> = ({ backgroundImagePath }) => {
     stampController.on("add", console.log);
     stampController.on("change", console.log);
     stampController.on("remove", console.log);
-
-    // StampFactory.prototype.create()は本来Backgroundのロードが完全に終了した後に
-    // 非同期的に呼び出されることを想定しているため、ここではsetTimeoutを用いて擬似的にBackground
-    // の読み込みが終わった後に非同期的に呼び出しています。
-    setTimeout(() => {
-      const stampTest1 = stampFactory.create("innocent");
-      const stampTest2 = stampFactory.create("innocent");
-      stampTest2.position.add(new Vector2(0, 1000));
-      stampController.add(stampTest1);
-      stampController.add(stampTest2);
-    }, 1000);
-
-    // TODO: 追加実装
 
     let requestId: number;
     const animate = () => {
