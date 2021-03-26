cc60ac0101c561510022b9496785494280e672d3
Koki Okada <uniokaworld@gmail.com>
2021-03-27 05:18:04 +0900

    Feat/take pic (#21)
    Co-authored-by: Shuntaro Nishizawa <me@shun.technology>

--- src/pages/Take.tsx
+++ src/pages/Take.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import type { BrowserRouterProps } from "react-router-dom";
+
 import { useEffect, useRef, useState } from "react";
 
 import { useStore } from "../lib/webrtc/store";
@@ -24,6 +25,8 @@ const CameraTest: FC = () => {
   } = useStore();
 
   const [streams, setStreams] = useState<Array<MediaStream>>([]);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const myVideoRef = useRef<HTMLVideoElement>(null);
   useEffect(() => {
@@ -50,6 +53,62 @@ const CameraTest: FC = () => {
     };
   }, [connectionController]);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) return;
+
+    const width = 960;
+    const height = 540;
+
+    assertNonNull(mediaStream);
+
+    const data = [...streams, mediaStream].map((stream) => {
+      const video = document.createElement("video");
+      video.autoplay = true;
+      video.srcObject = stream;
+
+      const tmpCanvas = document.createElement("canvas");
+      tmpCanvas.width = width;
+      tmpCanvas.height = height;
+      const tmpCtx = tmpCanvas.getContext("2d");
+      assertNonNull(tmpCtx);
+
+      return { video, tmpCtx };
+    });
+
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d");
+    assertNonNull(ctx);
+
+    let requestId: number;
+    const animate = () => {
+      ctx.clearRect(0, 0, width, height);
+      data.forEach(({ video, tmpCtx }) => {
+        tmpCtx.drawImage(video, 0, 0, width, height);
+        const imageData = tmpCtx.getImageData(0, 0, width, height);
+        const data = imageData.data;
+        const nn = width * height * 4;
+        for (let pi = 0; pi < nn; pi += 4) {
+          const r = data[pi];
+          const g = data[pi + 1];
+          const b = data[pi + 2];
+          if (r! >= 250 && g! >= 250 && b! >= 250) {
+            data[pi + 3] = 0;
+          }
+        }
+        tmpCtx.putImageData(imageData, 0, 0);
+        ctx.drawImage(tmpCtx.canvas, 0, 0, width, height);
+      });
+      requestId = requestAnimationFrame(animate);
+    };
+    animate();
+
+    return () => {
+      cancelAnimationFrame(requestId);
+    };
+  }, [mediaStream, streams, canvasRef]);
+
   return (
     <div>
       <div>
@@ -57,25 +116,8 @@ const CameraTest: FC = () => {
         <video autoPlay muted ref={myVideoRef} />
       </div>
       <div>
-        {streams.map((stream, index) => (
-          <div key={index}>
-            <Video stream={stream} />
-          </div>
-        ))}
+        <canvas ref={canvasRef} />
       </div>
     </div>
   );
-};
-
-type VideoProps = {
-  stream: MediaStream;
-};
-const Video: FC<VideoProps> = ({ stream }) => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    if (videoRef.current === null) return;
-    videoRef.current.srcObject = stream;
-  }, [videoRef]);
-
-  return <video autoPlay muted ref={videoRef} />;
 };
