4b964d7e3fc560eb71f90975a60bb32c34409d4c
Shuntaro Nishizawa <me@shun.technology>
2021-03-26 16:11:54 +0900

    feat: add listeners

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,23 +1,106 @@
 import type { FC } from "react";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 import { Scene } from "../lib/editor/Scene";
-import { Background } from "../lib/editor/objects";
+import {
+  Background,
+  Movable,
+  Stamp,
+  SerializableStamp,
+} from "../lib/editor/objects";
 import { MovableController } from "../lib/editor/controllers/MovableController";
 import { StampFactory } from "../lib/editor/factory/StampFactory";
+import { useStore } from "../lib/webrtc/store";
+import { assertNonNull } from "../utils/assert";
 
 import styles from "./Editor.module.scss";
 import { EmojiPaletteController } from "../lib/editor/controllers/EmojiPaletteController";
+
+type Message = {
+  type: "add" | "change" | "remove";
+  obj: string;
+};
 
 type Props = {
   backgroundImagePath: string;
 };
 const Editor: FC<Props> = ({ backgroundImagePath }) => {
+  const {
+    store: { connectionController },
+  } = useStore();
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [
+    movableController,
+    setMovableController,
+  ] = useState<MovableController | null>(null);
+
+  useEffect(() => {
+    if (movableController === null) return;
+    assertNonNull(connectionController);
+
+    const handleAdd = (obj: Movable) => {
+      connectionController.sendStampMessage(
+        JSON.stringify({
+          type: "add",
+          obj: obj.toJson(),
+        }),
+      );
+    };
+    const handleChange = (obj: Movable) => {
+      connectionController.sendStampMessage(
+        JSON.stringify({
+          type: "change",
+          obj: obj.toJson(),
+        }),
+      );
+    };
+    const handleRemove = (obj: Movable) => {
+      connectionController.sendStampMessage(
+        JSON.stringify({
+          type: "remove",
+          obj: obj.toJson(),
+        }),
+      );
+    };
+
+    const handleMessage = (message: string) => {
+      const data: Message = JSON.parse(message);
+      switch (data.type) {
+        case "add": {
+          const stamp = Stamp.fromJson(data.obj);
+          movableController.silentAdd(stamp);
+          break;
+        }
+        case "change": {
+          const serializedStamp: SerializableStamp = JSON.parse(data.obj);
+          const stamp = movableController.movables.children.find(
+            ({ id }) => id === serializedStamp.id,
+          );
+          assertNonNull(stamp);
+          stamp.fromJson(data.obj);
+          break;
+        }
+        // TODO: Remove処理
+      }
+    };
+
+    movableController.on("add", handleAdd);
+    movableController.on("change", handleChange);
+    movableController.on("remove", handleRemove);
+    connectionController.on("stampMessage", handleMessage);
+    return () => {
+      movableController.off("add", handleAdd);
+      movableController.off("change", handleChange);
+      movableController.off("remove", handleRemove);
+      connectionController.off("stampMessage", handleMessage);
+    };
+  }, [movableController, connectionController]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas === null) return;
+    if (movableController !== null) return;
 
     const scene = new Scene(canvas);
     const background = new Background(canvas, backgroundImagePath);
@@ -39,6 +122,8 @@ const Editor: FC<Props> = ({ backgroundImagePath }) => {
     stampController.on("change", console.log);
     stampController.on("remove", console.log);
 
+    setMovableController(stampController);
+
     let requestId: number;
     const animate = () => {
       scene.draw();
@@ -50,7 +135,7 @@ const Editor: FC<Props> = ({ backgroundImagePath }) => {
       stampController.deconstructor();
       emojiPalette.destruct();
     };
-  }, [canvasRef, backgroundImagePath]);
+  }, [canvasRef, setMovableController, backgroundImagePath]);
 
   return <canvas className={styles["editor"]} ref={canvasRef} />;
 };
