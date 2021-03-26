ecf67682536cac1c686d1f47da8d6d040c9f682f
koki okada <uniokaworld@gmail.com>
2021-03-27 03:17:07 +0900

    fix: ctxList

--- src/pages/Take.tsx
+++ src/pages/Take.tsx
@@ -11,12 +11,35 @@ const Take: FC<BrowserRouterProps> = () => {
   const setmyCamera = (MyStream: MediaStream) => {
     setCameraList([...cameraList, MyStream]);
   };
+  useEffect(() => {
+    if (cameraList[0] == null) {
+      return;
+    }
+    if (cameraList[1] != null) {
+      return;
+    }
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          width: { ideal: 960 },
+          height: { ideal: 540 },
+          deviceId: {
+            exact:
+              "62ee584f7ebca93c9d91c3a48fbd72721a3f2a9c7a0ad4efcca5ef2311990a55",
+          },
+        },
+      })
+      .then((stream) => {
+        setCameraList([...cameraList, stream]);
+      });
+  }, [cameraList]);
 
   return (
     <div>
       <h1>This is Take page.</h1>
-      <Pic MyStream={setmyCamera} />
       <Chromakey MediaStream={cameraList} />
+      <Pic MyStream={setmyCamera} />
     </div>
   );
 };
