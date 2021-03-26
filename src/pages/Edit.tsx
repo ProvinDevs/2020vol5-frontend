584f8a260a15bba4b12539b4b2a30e615485ef4f
unios103 <unios103@gmail.com>
2021-03-27 01:24:59 +0900

    modal

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,19 +1,14 @@
 import type { FC } from "react";
 import type { BrowserRouterProps } from "react-router-dom";
-import { useState } from "react";
 
 import Editor from "../components/Editor";
-import Modal from "../components/common/Modal";
 
 import styles from "./Edit.module.scss";
 import mockedImage from "../mock/image.jpg";
 
 const Edit: FC<BrowserRouterProps> = () => {
-  const [isClose, setMenuState] = useState<boolean>(true);
   return (
     <div>
-      <Modal isClose={isClose} setMenuState={setMenuState} />
-      <button onClick={() => setMenuState(!isClose)}>開く</button>
       <div className={styles["editor_wrapper"]}>
         <Editor backgroundImagePath={mockedImage} />
       </div>
